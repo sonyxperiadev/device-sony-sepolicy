d09787fae56f3f2d4650cc980250a79672558ab4
Humberto Borba <humberos@omnirom.org>
2018-09-12 18:43:10 -0300

    sony: sepolicy: Rebase sepolicy for project treble rules
    Change-Id: I6bd92002fac3dd3cf040110a326f346e2ad8cbbf
    Signed-off-by: Humberto Borba <humberos@omnirom.org>

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,1 +1,16 @@
-BOARD_SEPOLICY_DIRS += device/sony/sepolicy
+# Sony Open Devices
+# SELinux policy variable definitions
+LOCAL_SEPOLICY := device/sony/sepolicy
+
+BOARD_SEPOLICY_DIRS += \
+    $(LOCAL_SEPOLICY)/vendor
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    $(LOCAL_SEPOLICY)/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(LOCAL_SEPOLICY)/private
+
+# Add sepolicy version to support OS upgrade and backward compatibility
+BOARD_SEPOLICY_VERS := \
+    $(PLATFORM_SDK_VERSION).0
