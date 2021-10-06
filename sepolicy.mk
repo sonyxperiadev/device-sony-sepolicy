c8ef351aa22bb339333fe94eb37cfd6f1d26edb8
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-10-06 11:34:35 +0200

    sepolicy.mk: Follow rename change from upstream
    In Android S (12) the BOARD_PLAT_*_SEPOLICY_DIR variables
    have been renamed to SYSTEM_EXT_*_SEPOLICY_DIRS.
    
    See for reference: https://android-review.googlesource.com/q/topic:board-system-ext-sepolicy

--- sepolicy.mk
+++ sepolicy.mk
@@ -5,13 +5,8 @@ LOCAL_SEPOLICY := device/sony/sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(LOCAL_SEPOLICY)/vendor
 
-# TODO(b/systemext-policy): Will be renamed to:
-# - SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS
-# - SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS
-# once Android S lands and throw a warning/error
-# See https://r.android.com/q/topic:board-system-ext-sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     $(LOCAL_SEPOLICY)/system_ext/private
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     $(LOCAL_SEPOLICY)/system_ext/public
